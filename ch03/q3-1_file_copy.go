1ef501de404801157220fb848e8344cb2c044cad
kakts <kakts.git@gmail.com>
2023-10-02 23:47:32 +0900

    feat: 3.9 q3-1 file copy

---
+++ ch03/q3-1_file_copy.go
@@ -0,0 +1,25 @@
+// 3.9 Q3.1 ファイルコピー
+
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("old.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	newFile, err := os.Create("new.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer newFile.Close()
+
+	io.Copy(newFile, file)
+
+}
