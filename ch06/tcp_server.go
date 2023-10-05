7f3f251f4e9ffe7d0a582e86d2a0d4959395eb36
kakts <kakts.git@gmail.com>
2023-10-05 23:56:25 +0900

    feat: 6.4 tcp server improved

--- ch06/tcp_server.go
+++ ch06/tcp_server.go
@@ -9,10 +9,19 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer ln.Close()
-	_, err = ln.Accept()
-	if err != nil {
-		panic(err)
-	}
 
+	// リクエストを受け取って1回で終了しないために、Accept()を何度も呼ぶ
+	for {
+		_, err := ln.Accept()
+		if err != nil {
+			// handle error
+		}
+
+		// 1リクエスト処理中に他のリクエストのAccept()が実行できるように、goroutineを使って非同期にレスポンス処理する
+		go func() {
+			// connを使った読み書き
+
+		}()
+
+	}
 }
